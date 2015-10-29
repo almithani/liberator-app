1f77a2e5b5c4d586624d34e1e4c98fd7e3ef9658
Al Mithani <almithani@gmail.com>
2015-10-29 16:45:32 -0700

    updating lightbox styles"

--- js/lightbox.js
+++ js/lightbox.js
@@ -4,92 +4,94 @@ window.Lightbox = React.createClass({
 		Use property closeLightbox to bind to the close event.
 	*/
 
-    contentStyles: {
-        position: 'fixed',
-        top: '10%',
-        bottom: '10%',
-        left: '20%',
-        right: '20%',
-        backgroundColor: '#fff',
-        color: '#7F7F7F',
-        padding: '20px',
-        border: '2px solid #ccc',
-        borderRadius: '20px',
-        boxShadow: '0 1px 5px #333',
-        zIndex:'101'
-    },
+	contentStyles: {
+		position: 'fixed',
+		top: '10%',
+		bottom: '10%',
+		left: '20%',
+		right: '20%',
+		backgroundColor: '#fff',
+		borderRadius: '3px',
+		zIndex:'101',
+		padding: '10px'
+	},
 
-    overlayStyles: {
-        background: 'black',
-        opacity: '.5',
-        position: 'fixed',
-        top: '0px',
-        bottom: '0px',
-        left: '0px',
-        right: '0px',
-        zIndex: '100'
-    },
+	overlayStyles: {
+		background: 'black',
+		opacity: '.5',
+		position: 'fixed',
+		top: '0px',
+		bottom: '0px',
+		left: '0px',
+		right: '0px',
+		zIndex: '100'
+	},
 
-    closeTagStyles: {
-        float: 'right',
-        marginTop: '-30px',
-        marginRight: '-30px',
-        cursor: 'pointer',
-        color: '#fff',
-        border: '1px solid #AEAEAE',
-        borderRadius: '30px',
-        background: '#605F61',
-        fontSize: '31px',
-        fontWeight: 'bold',
-        display: 'inline-block',
-        lineHeight: '0px',
-        padding: '11px 3px',
-        textDecoration: 'none'
-    },
+	closeTagStyles: {
+		float: 'right',
+		marginTop: '-15px',
+		marginRight: '-20px',
+		cursor: 'pointer',
+		color: '#fff',
+		border: '1px solid #000',
+		borderRadius: '6px',
+		background: '#FF4751',
+		fontSize: '1.5em',
+		lineHeight: '0.5em',
+		fontWeight: 'bold',
+		display: 'inline-block',
+		padding: '3px 5px 5px 5px',
+		textDecoration: 'none'
+	},
 
-    render: function(){
-        return (
-            <div>
-                <div style={this.overlayStyles} onClick={this.props.closeLightbox} />
-                <div style={this.contentStyles}>
-                    <a style={this.closeTagStyles} onClick={this.props.closeLightbox}>x</a>
-                    {this.props.children}
-                </div>
-        	</div>
-        );
-    }  
+	render: function(){
+		return (
+			<div>
+				<div style={this.overlayStyles} onClick={this.props.closeLightbox} />
+				<div style={this.contentStyles}>
+					<a style={this.closeTagStyles} onClick={this.props.closeLightbox}>x</a>
+					{this.props.children}
+				</div>
+			</div>
+		);
+	}  
 });
 
 
 window.BookLightbox = React.createClass({
 
-	Book: {},	//private var	
+	Book: {},
+   
+	epubOptions: {
+		restore: true
+	},	//private var	
 
-    prevPage: function() {
-    	this.Book.prevPage();
-    },
+	prevPage: function() {
+		this.Book.prevPage();
+	},
 
-    nextPage: function() {
-    	this.Book.nextPage();
-    },
+	nextPage: function() {
+		this.Book.nextPage();
+	},
 
-    closeLightbox: function() {
-    	this.props.setActiveItem(null);
-    },	
+	closeLightbox: function() {
+		this.props.setActiveItem(null);
+	},	
 
 	componentDidUpdate: function() {
 		//this.Book = ePub("data/melville_moby-dick.epub");
 		if( this.props.activeItem != null ) {
-			this.Book = ePub(this.props.activeItem.epub);
+			var opts = _.clone(this.epubOptions);
+			this.Book = ePub( this.props.activeItem.epub, opts );
 			this.Book.renderTo("epubReader");  
 		}
-    },
+	},
 
 	render: function() {
-        if ( this.props.activeItem != null ){
-            return (
-                <Lightbox closeLightbox={this.closeLightbox}>
-                	<div className="book">
+		if ( this.props.activeItem != null ){
+			return (
+				<Lightbox closeLightbox={this.closeLightbox}>
+					<div className="book">
 						<h4 className="book-title">
 							{this.props.activeItem.title} 
 							<span className="book-author"> - {this.props.activeItem.author}</span>
@@ -100,11 +102,11 @@ window.BookLightbox = React.createClass({
 							<div className="btn-page next" onClick={this.nextPage}>&gt;</div>
 						</div>
 					</div>
-            	</Lightbox>
-            );
-        } else {
-            return (<div></div>);
-        }
+				</Lightbox>
+			);
+		} else {
+			return (<div></div>);
+		}
 	}
 
 });
